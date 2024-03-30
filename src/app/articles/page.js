5de7e3262ddcbf2eb0448d9e5892cc75c75982fe
hakan <hbilgic1992@gmail.com>
2024-03-30 17:06:43 +0100

    debug codesnippet mobile responsiveness with grid layout

--- src/app/articles/page.js
+++ src/app/articles/page.js
@@ -41,7 +41,7 @@ async function Blog() {
                       href={`/articles/${post.slug}`}
                     >
                       <div className="flex items-start justify-start py-4 px-2 -ml-2 w-fit md:w-[45rem] rounded-lg bg-transparent text-[var(--text-color-secondary-700)] hover:text-yellow-600 hover:bg-[var(--text-color-primary-100)] duration-500   ">
-                        <p className="min-w-14 mr-8 mt-[2px] text-sm text-[var(--text-color-secondary-500)]">
+                        <p className="min-w-14 mr-8 mt-[2px] text-sm text-[var(--text-color-primary-600)]">
                           {format(
                             new Date(Date.parse(post.publishedOn)),
                             'MMM dd'
