1252ff978c11c69e010d0b9f1e32a0864875befc
hakan <hbilgic1992@gmail.com>
2024-04-14 09:40:14 +0200

    change lighting in landingpage

--- src/components/landingpage/elephant_model/elephant.jsx
+++ src/components/landingpage/elephant_model/elephant.jsx
@@ -1,40 +1,41 @@
-import { useGLTF, Gltf, useAnimations } from '@react-three/drei';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import { usePathname } from 'next/navigation';
 import React, { useEffect } from 'react';
 
 function Elephant() {
   const { nodes, materials, animations } = useGLTF('/models/Elephant2.glb');
   const { ref, actions } = useAnimations(animations);
+  const pathname = usePathname();
+
   console.log(nodes, 'nodes');
   console.log(materials, 'materials');
   console.log(animations, 'animations');
 
   useEffect(() => {
-    actions?.EsqueletoAction.play();
-  });
+    const action = actions?.EsqueletoAction;
+
+    if (pathname === '/') {
+      action.reset().play();
+      console.log(pathname, 'pathname triggered');
+    }
+    return () => {
+      action?.stop();
+    };
+  }, [actions, pathname]);
 
   return (
     <>
-      <group
-        ref={ref}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, -2, 0]}
-        rotation={[0, 0, 0]}
-      >
-        {/* <primitive object={nodes.Scene.children[10]} /> */}
+      <ambientLight intensity={0.2} />
+      <directionalLight position={[0, 10, 5]} intensity={6} />
 
+      <group ref={ref} scale={0.7} position={[0, -2, 0]} rotation={[0, 0, 0]}>
         {nodes.Scene.children.map((child) => {
           return <primitive key={child.uuid} object={child} />;
         })}
       </group>
-      {/* <Gltf
-        ref={ref}
-        src="/models/elephant.glb"
-        scale={0.01}
-        position={[0, -2, 0]}
-        rotation={[0, 0, 0]}
-      /> */}
     </>
   );
 }
 
 export default Elephant;
+useGLTF.preload('/models/Elephant2.glb');
