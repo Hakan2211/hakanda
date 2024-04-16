9a3cfd6879ab50ffe97c43560e17f75ef0db78ec
hakan <hbilgic1992@gmail.com>
2024-04-16 18:35:25 +0200

    mouse cursor follow with spring animation

--- src/components/landingpage/canvas.jsx
+++ src/components/landingpage/canvas.jsx
@@ -10,7 +10,14 @@ function CanvasLanding() {
   return (
     <div className="h-[100svh] fixed top-0 left-0 right-0">
       <Canvas>
-        <OrbitControls />
+        <OrbitControls
+          maxDistance={6}
+          minDistance={4}
+          maxAzimuthAngle={Math.PI / 4}
+          minAzimuthAngle={-Math.PI / 4}
+          minPolarAngle={Math.PI / 4}
+          maxPolarAngle={Math.PI / 2}
+        />
         <ambientLight intensity={1.5} />
         <pointLight position={[10, 10, 10]} />
         <Suspense>
--- src/components/landingpage/elephant_model/elephant.jsx
+++ src/components/landingpage/elephant_model/elephant.jsx
@@ -3,12 +3,18 @@ import useViewport from '@/hooks/useViewport';
 import { useGLTF, useAnimations } from '@react-three/drei';
 import { usePathname } from 'next/navigation';
 import React, { useEffect } from 'react';
+import { useSpring, a } from '@react-spring/three';
 
 function Elephant() {
   const { scene, animations } = useGLTF('/models/Elephant2.glb');
   const { ref, actions } = useAnimations(animations);
   const pathname = usePathname();
   const viewport = useViewport();
+
+  const [{ rotation }, api] = useSpring(() => ({
+    rotation: [0, 0, 0],
+    config: { mass: 2, tension: 120, friction: 100 },
+  }));
 
   useEffect(() => {
     const action = actions?.EsqueletoAction;
@@ -18,10 +24,33 @@ function Elephant() {
     } else {
       action.stop();
     }
+
+    function handleMouseMove(event) {
+      const x = (event.clientX / window.innerWidth) * 2 - 1;
+      const y = -(event.clientY / window.innerHeight) * 2 + 1;
+      const distance = Math.sqrt(x * x + y * y);
+      const activeRadius = 0.5;
+
+      if (distance < activeRadius) {
+        const horizontalRotation = Math.max(
+          -Math.PI / 8,
+          Math.min(Math.PI / 8, (x * Math.PI) / 4)
+        );
+        const verticalRotation = Math.max(
+          -Math.PI / 16,
+          Math.min(Math.PI / 16, (-y * Math.PI) / 8)
+        );
+        api.start({ rotation: [verticalRotation, horizontalRotation, 0] });
+      }
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
     return () => {
       action?.stop();
+      window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [actions, pathname]);
+  }, [actions, pathname, api]);
 
   const scale =
     viewport === 'mobile' ? 0.76 : viewport === 'laptop' ? 0.79 : 0.8;
@@ -32,17 +61,17 @@ function Elephant() {
     <>
       <ambientLight intensity={0.2} />
       <directionalLight position={[0, 10, 5]} intensity={6} />
-      <group
+      <a.group
         ref={ref}
         scale={scale}
         position={[0, positionY, 0]}
-        rotation={[0, 0, 0]}
+        rotation={rotation}
       >
         {/* {nodes.Scene.children.map((child) => {
           return <primitive key={child.uuid} object={child} />;
         })} */}
         <primitive object={scene} />
-      </group>
+      </a.group>
     </>
   );
 }
