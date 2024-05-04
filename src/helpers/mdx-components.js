e79867953e1bf409a31fd7f8df6d7b62f7d9aed2
hakan <hbilgic1992@gmail.com>
2024-05-04 18:36:51 +0200

    added the testcomponent

--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,8 +1,9 @@
+import dynamic from 'next/dynamic';
+
 import AlertText from '@/components/AlertText/alertText';
 import CodeSnippet from '@/components/CodeSnippet';
-import TestComponent from '@/components/TestComponent/testComponent';
-
 import BlogHeading from '@/components/blogheading/blogheading';
+import TestComponent from '@/components/TestComponent/testComponent';
 
 const COMPONENT_MAP = {
   pre: CodeSnippet,
