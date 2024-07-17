e7d937f496a126b0c140aba33cd85525e3d7b262
hakan <hbilgic1992@gmail.com>
2024-07-18 01:05:58 +0200

    test

--- src/app/articles/[postSlug]/opengraph-image.js
+++ src/app/articles/[postSlug]/opengraph-image.js
@@ -1,18 +1,40 @@
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { ImageResponse } from 'next/og';
 
+export const runtime = 'edge';
 export const size = {
   width: 1200,
   height: 630,
 };
+export const alt = 'About Acme';
 
 export default async function Image({ params }) {
   const blogPostData = await loadBlogPost(params.postSlug);
   const { frontmatter } = blogPostData;
 
   console.log('frontmatter.title', frontmatter.title);
-
-  return new ImageResponse(<img alt="og-image" src={frontmatter.image} />, {
-    ...size,
-  });
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        About Acme
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
 }
