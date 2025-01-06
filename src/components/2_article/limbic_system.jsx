48dc1af73e47cf549dc10439c7ce90223878a952
hakan <hbilgic1992@gmail.com>
2025-01-06 11:02:24 +0100

    change constants Blog description

--- src/components/2_article/limbic_system.jsx
+++ src/components/2_article/limbic_system.jsx
@@ -1,28 +1,27 @@
-"use client";
-import { OrbitControls, Text } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { BrainModel } from "./brain_model";
-import { color } from "framer-motion";
+'use client';
+import { OrbitControls, Text } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { BrainModel } from './brain_model';
 
 function LimbicSystem() {
   const annotations = [
-    { name: "Amygdala", position: [-10, -10, 0], color: "#8e151f" },
-    { name: "Hippocampus", position: [25, -10, 0], color: "#4C8830" },
+    { name: 'Amygdala', position: [-10, -10, 0], color: '#8e151f' },
+    { name: 'Hippocampus', position: [25, -10, 0], color: '#4C8830' },
     {
-      name: "Cingulate Cortex",
+      name: 'Cingulate Cortex',
       position: [7, -15, 20],
-      color: "#E2505C",
+      color: '#E2505C',
     },
-    { name: "Thalamus", position: [5, -20, 5], color: "#148f18" },
-    { name: "Hypothalamus", position: [5, -20, -5], color: "#8f686a" },
+    { name: 'Thalamus', position: [5, -20, 5], color: '#148f18' },
+    { name: 'Hypothalamus', position: [5, -20, -5], color: '#8f686a' },
   ];
   return (
-    <div className="my-10" style={{ height: "600px", borderRadius: "8px" }}>
+    <div className="my-10" style={{ height: '600px', borderRadius: '8px' }}>
       <Canvas
         style={{
-          height: "100%",
-          borderRadius: "8px",
-          background: "var(--canvas-bg-color)",
+          height: '100%',
+          borderRadius: '8px',
+          background: 'var(--canvas-bg-color)',
         }}
         //shadows
         // camera={{ position: viewport === 'mobile' ? [3, 1, 22] : [0, 1, 17] }}
@@ -34,7 +33,7 @@ function LimbicSystem() {
           position={[1, 6, 4]}
           intensity={1}
           castShadow
-          color={"white"}
+          color={'white'}
         />
         <BrainModel
           annotations={annotations}
